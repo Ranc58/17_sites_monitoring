97648e4672a9920d834a5b4f20ea7836949ff9fa
Ranc58 <rancvova@gmail.com>
2017-06-12 16:15:04 +0300

    refactor check_sites_health.py for new funcs and edit old funcs to more reusable code init README.md

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # Sites Monitoring Utility
+This program check response of site and domain experation date.
 
-[TODO. There will be project description]
+# How to install:
+1. Recomended use venv or virtualenv for better isolation.\
+   Venv setup example: \
+   `python3 -m venv myenv`\
+   `source myenv/bin/activate`
+2. Install requirements: \
+   `pip3 install -r requirements.txt` (alternatively try add `sudo` before command)
+3. Put list of URLs to file `sites.txt` in script folder. **URLs must be in each line one by one**!
 
+# Launch example.
+
+```
+$ python3 check_sites_health.py
+
+Url: https://google.com
+Server response: 200
+To end of domen expiration: 39 months
+
+Url: https://devman.org
+Server response: 200
+To end of domen expiration: 2 months
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- check_sites_health.py
+++ check_sites_health.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta
 import requests
 import whois
 
+FILEPATH = 'sites.txt'
+
 
 def create_url_list(urls):
     url_list = [url.strip() for url in urls]
@@ -21,8 +23,8 @@ def get_server_response(url_list):
 def get_domain_expiration_date(url_list):
     urls_expiration_date_list = []
     for url in url_list:
-        w = whois.whois(url)
-        expiration_date = w.expiration_date
+        domain = whois.whois(url)
+        expiration_date = domain.expiration_date[0]
         exp_date = {'exp_date': expiration_date}
         urls_expiration_date_list.append(exp_date)
     return urls_expiration_date_list
@@ -34,21 +36,37 @@ def get_full_info_list(response_list, exp_date_list):
     return full_info_list
 
 
-def output_urls_info(full_info_list):
+def get_output_urls_info(full_info_list):
+    output_info_list = []
     for list in full_info_list:
         date_diff = list['exp_date'] - datetime.today()
         months_diff = int(int(date_diff.days) / 30)
+        site = {'url': list['url'],
+                'response_code': list['response'],
+                'expiration': months_diff}
+        output_info_list.append(site)
+    return output_info_list
+
+
+def print_urls_info(output_info_list):
+    for url in output_info_list:
         print("\nUrl: {}\n"
               "Server response: {}\n"
               "To end of domen expiration: {} months"
-              .format(list['url'], list['response'], months_diff))
+              .format(url['url'], url['response_code'], url['expiration']))
 
 
 if __name__ == '__main__':
-    filepath = 'sites.txt'
-    with open(filepath) as urls:
-        url_list = create_url_list(urls)
-    response_list = get_server_response(url_list)
-    exp_date_list = get_domain_expiration_date(url_list)
-    full_info_list = get_full_info_list(response_list, exp_date_list)
-    output_urls_info(full_info_list)
+    try:
+        with open(FILEPATH) as urls:
+            url_list = create_url_list(urls)
+        response_list = get_server_response(url_list)
+    except FileNotFoundError as error:
+        print(error)
+    except requests.exceptions.ConnectionError:
+        print('Error! Please check "sites.txt" for correct URLs!')
+    else:
+        exp_date_list = get_domain_expiration_date(url_list)
+        full_info_list = get_full_info_list(response_list, exp_date_list)
+        output_info_list = get_output_urls_info(full_info_list)
+        print_urls_info(output_info_list)
