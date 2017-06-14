b2359d0d3f338dd2db841f1866bbe87cb17dc27f
Ranc58 <rancvova@gmail.com>
2017-06-14 14:34:14 +0300

    Edit check_sites_health.py for right argparse argument Add some try..except blocks

--- check_sites_health.py
+++ check_sites_health.py
@@ -34,23 +34,28 @@ def get_output_urls_info(response_list, exp_date):
 
 def create_parser_for_user_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', help='path to file with list of URLs', type=str)
-    return parser.parse_args()
+    parser.add_argument('-f', '--file', required=True,
+                        help='path to file with list of URLs',
+                        type=argparse.FileType())
+    namespace = parser.parse_args()
+    return namespace
 
 
 if __name__ == '__main__':
     namespace = create_parser_for_user_arguments()
-    filepath = namespace.file
-    try:
-        with open(filepath) as urls:
-            url_list = create_url_list(urls)
-        for url in url_list:
+    filepath = namespace.file.readlines()
+    url_list = create_url_list(filepath)
+    for url in url_list:
+        try:
             response_list = get_server_response(url)
+        except (requests.exceptions.ConnectionError):
+            print('\nError!\nPlease check that URL for correctness: {}'
+                  .format(url))
+        else:
             exp_date_list = get_domain_expiration_date(url)
-            output_info_list = get_output_urls_info(response_list,exp_date_list)
+            output_info_list = get_output_urls_info(response_list,
+                                                    exp_date_list)
             print("\nUrl: {url}\n"
                   "Server response: {response_code}\n"
                   "To end of domen expiration: {expiration} months"
                   .format(**output_info_list))
-    except (FileNotFoundError, requests.exceptions.ConnectionError) as error:
-        print(error)
